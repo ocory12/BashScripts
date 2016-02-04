dd8268fef60ba3b9d3b7373953ca1e6fde3b98cc
Oliver Cory <ocory12@lx1816.dovergramboys.kent.sch.uk>
2016-02-04 11:43:14 +0000

    Added Extension Task To DiceStuff.sh

--- DiceStuff.sh
+++ DiceStuff.sh
@@ -3,32 +3,42 @@ declare -i DiceOne=0
 declare -i DiceTwo=0
 declare -t UserGuess=0
 declare -i DiceTotal=0
-
-
-
+declare -i tries=0
 
 let DiceOne=$(( ( RANDOM %6) +1))
-		
 let DiceOne=$(( ( RANDOM %6) +1))
-		
-		
-
-
 let DiceTotal=$DiceOne+$DiceTwo
 
-echo "What Is Your Guess?"
-read UserGuess
+echo "Start Game Now"
+
+while [ $UserGuess -ne $DiceTotal -a $tries -lt 10 ]
+
+do
+
+
+			echo "What Is Your Guess?"
+			read UserGuess
 
 
 
-if [ $UserGuess == $DiceTotal ] 
-	then 
-		echo "You Guest Right"
-		
-elif [ $UserGuess -gt $DiceTotal ]
-	then
-		echo "To High"
-	else 
-		echo "To Low"
-		
+			if [ $UserGuess == $DiceTotal ]
+				then
+					echo "You Guest Right"
+
+			elif [ $UserGuess -gt $DiceTotal ]
+				then
+					echo "To High"
+			else
+					echo "To Low"
+
+			fi
+
+			let tries=$tries+1
+done
+
+if [ $UserGuess == $DiceTotal ]
+then
+	echo "You Win"
+else
+	echo "You Have Failed"
 fi
