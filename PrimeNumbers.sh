23ea5034f731ac1e914b49b8cb38d8c707e418ab
Oliver Cory <ocory12@lx1816.dovergramboys.kent.sch.uk>
2016-02-08 11:21:23 +0000

    Added Stuff

--- PrimeNumbers.sh
+++ PrimeNumbers.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+#Copyright (c) 2016 Ollie Cory All Rights Reserved.
+
+
 declare -i UserInput=0
 declare -i Prime=0
-
-
+declare -i Counter=2
+declare -i result=0
 
 echo "Enter A Number"
 read UserInput
 
-for i in $( seq 1 $UserInput )
+while [ $Counter -lt $UserInput ]
+
   do
-    
+
+    echo "Counter Is $Counter and result is $result"
+
+    result=$(awk "BEGIN {print $UserInput/$Counter}")
+
+
+
+    let Counter=$Counter+1
+
+done
